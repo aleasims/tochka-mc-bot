c8bee95d69c1c4a6ca200efaa8fc6724f87568af
Alexander Evgin <aleasims@gmail.com>
2022-09-21 02:36:57 +0300

    fix some docs

--- panel/views.py
+++ panel/views.py
@@ -1,15 +1,15 @@
-# export PYTHONPATH="/Users/chukrello/proga/tochka-mc-bot/"
-from django.http import HttpResponse, HttpRequest
+from rest_framework import permissions, viewsets
 
-from rest_framework import viewsets
-from rest_framework import permissions
+from .models import (Application, Course, Message, ScheduledMessage, TGAdmin,
+                     User)
+from .serializers import (ApplicationSerializer, CourseSerializer,
+                          MessageSerializer, ScheduledMessageSerializer,
+                          TGAdminSerializer, UserSerializer)
 
-from .models import TGAdmin, User, Course, Application, Message, ScheduledMessage
-from .serializers import TGAdminSerializer, UserSerializer, CourseSerializer, ApplicationSerializer, MessageSerializer, ScheduledMessageSerializer
 
 class TGAdminViewSet(viewsets.ModelViewSet):
     """
-    API endpoint that allows users to be viewed or edited.
+    API endpoint that allows TGAdmins to be viewed or edited.
     """
     queryset = TGAdmin.objects.all().order_by('-name')
     serializer_class = TGAdminSerializer
@@ -18,31 +18,34 @@ class TGAdminViewSet(viewsets.ModelViewSet):
 
 class UserViewSet(viewsets.ModelViewSet):
     """
-    API endpoint that allows users to be viewed or edited.
+    API endpoint that allows Users to be viewed or edited.
     """
     queryset = User.objects.all().order_by('-name')
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
 
+
 class CourseViewSet(viewsets.ModelViewSet):
     """
-    API endpoint that allows users to be viewed or edited.
+    API endpoint that allows Courses to be viewed or edited.
     """
-    queryset = Course.objects.all().order_by('-name')
+    queryset = Course.objects.all().order_by('-order')
     serializer_class = CourseSerializer
     permission_classes = [permissions.IsAuthenticated]
 
+
 class ApplicationViewSet(viewsets.ModelViewSet):
     """
-    API endpoint that allows users to be viewed or edited.
+    API endpoint that allows Applications to be viewed or edited.
     """
     queryset = Application.objects.all()
     serializer_class = ApplicationSerializer
     permission_classes = [permissions.IsAuthenticated]
 
+
 class MessageViewSet(viewsets.ModelViewSet):
     """
-    API endpoint that allows users to be viewed or edited.
+    API endpoint that allows Messages to be viewed or edited.
     """
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
@@ -51,7 +54,7 @@ class MessageViewSet(viewsets.ModelViewSet):
 
 class ScheduledMessageViewSet(viewsets.ModelViewSet):
     """
-    API endpoint that allows users to be viewed or edited.
+    API endpoint that allows ScheduledMessages to be viewed or edited.
     """
     queryset = ScheduledMessage.objects.all()
     serializer_class = ScheduledMessageSerializer
