720dd1ac10ed45fcbc1623a0fc70932b80d2b021
Alexander Evgin <aleasims@gmail.com>
2022-09-21 02:36:36 +0300

    extend api serializers

--- panel/serializers.py
+++ panel/serializers.py
@@ -1,35 +1,41 @@
-from .models import TGAdmin, User, Course, Application, Message, ScheduledMessage
 from rest_framework import serializers
+
+from .models import (Application, Course, Message, ScheduledMessage, TGAdmin,
+                     User)
+
 
 class TGAdminSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = TGAdmin
         fields = ['tg_id', 'name']
 
+
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ['tg_id', 'name']
+        fields = ['tg_id', 'name', 'surname', 'group_id']
+
 
 class CourseSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Course
-        fields = ['name', 'description', 'order']
+        fields = ['id', 'name', 'description', 'who',
+                  'where', 'day', 'time', 'img_path', 'order']
+
 
 class ApplicationSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Application
-        fields = ['user', 'course']
+        fields = ['id', 'user', 'course']
+
 
 class MessageSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Message
-        fields = ['text']
+        fields = ['id', 'text']
 
 
 class ScheduledMessageSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = ScheduledMessage
-        fields = ['message', 'recipient']
-
-
+        fields = ['id', 'message', 'recipient']
