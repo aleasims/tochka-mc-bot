37950f9de998c21b3253e1f9afed460d23793e14
Alexander Evgin <aleasims@gmail.com>
2022-09-21 04:35:13 +0300

    front fetch data from api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -22,8 +22,8 @@ export default class App extends React.Component {
   }
 
   async componentDidMount() {
+    await this.getColumns();
     this.getData();
-    this.getColumns();
   }
 
   async getColumns() {
@@ -34,29 +34,50 @@ export default class App extends React.Component {
     const courseList = await res.json();
     console.log(courseList);
 
-    let columns = [
-      {
-        Header: "First Name",
-        accessor: "firstName",
-        sortable: false,
+    var columns_ = [];
+    columns_.push({
+      Header: "Имя",
+      accessor: "name",
+      sortable: true,
+      show: true,
+      displayValue: "Имя"
+    });
+
+    for (var i = 0; i < courseList.length; i++)
+    {
+      columns_.push({
+        Header: courseList[i].name,
+        accessor: courseList[i].id.toString(),
+        sortable: true,
         show: true,
-        displayValue: " First Name"
-      },
-      {
-        Header: "Status",
-        accessor: "status",
-        sortable: false,
-        show: true,
-        displayValue: "Status "
-      },
-      {
-        Header: "Visits",
-        accessor: "visits",
-        sortable: false,
-        show: true,
-        displayValue: " Visits "
-      }
-    ];
+        displayValue: courseList[i].name,
+      });
+    }
+
+    // let columns = [
+    //   {
+    //     Header: "First Name",
+    //     accessor: "firstName",
+    //     sortable: false,
+    //     show: true,
+    //     displayValue: " First Name"
+    //   },
+    //   {
+    //     Header: "Status",
+    //     accessor: "status",
+    //     sortable: false,
+    //     show: true,
+    //     displayValue: "Status "
+    //   },
+    //   {
+    //     Header: "Visits",
+    //     accessor: "visits",
+    //     sortable: false,
+    //     show: true,
+    //     displayValue: " Visits "
+    //   }
+    // ];
+    let columns = columns_;
     this.setState({ columns });
   }
 
@@ -66,24 +87,48 @@ export default class App extends React.Component {
     }
     const res = await fetch(`${API_URL}/users`);
     const userList = await res.json();
+    // const res2 = await fetch(`${API_URL}/applications`);
+    // const applicList = await res.json();
     console.log(userList);
 
-    let data = [
-      { firstName: "aaaaa", status: "Pending", visits: 155 },
-      { firstName: "aabFaa", status: "Pending", visits: 155 },
-      { firstName: "adaAAaaa", status: "Approved", visits: 1785 },
-      { firstName: "aAaaaa", status: "Approved", visits: 175 },
-      { firstName: "adaSaaa", status: "Cancelled", visits: 165 },
-      { firstName: "aasaaa", status: "Cancelled", visits: 157 },
-      { firstName: "aweaaaaaewea", status: "Approved", visits: 153 },
-      { firstName: "adaAAadsdweaa", status: "Approved", visits: 17585 },
-      { firstName: "aAaaaa", status: "Approved", visits: 175 },
-      { firstName: "adadsdSaaa", status: "Cancelled", visits: 165 },
-      { firstName: "dsdcdaaaaa", status: "Cancelled", visits: 157 },
-      { firstName: "aaadvsaa", status: "Submitted", visits: 5153 },
-      { firstName: "aaaaswea", status: "Pending", visits: 1555 },
-      { firstName: "aaaaauwe", status: "Submitted", visits: 155 }
-    ];
+    var data_ = [];
+    for (var i = 0; i < userList.length; i++)
+    {
+      const user = {};
+      user.name = userList[i].name + " " + userList[i].surname;
+      for (var j = 1; j < this.state.columns.length; j++)
+      {
+        user[this.state.columns[j].accessor] = "+";
+      }
+      console.log(user);
+      data_.push(user);
+      // columns_.push({
+      //   Header: courseList[i].name,
+      //   accessor: courseList[i].id,
+      //   sortable: true,
+      //   show: true,
+      //   displayValue: courseList[i].name,
+      // });
+    }
+    console.log(data_);
+
+    // let data = [
+    //   { firstName: "aaaaa", status: "Pending", visits: 155 },
+    //   { firstName: "aabFaa", status: "Pending", visits: 155 },
+    //   { firstName: "adaAAaaa", status: "Approved", visits: 1785 },
+    //   { firstName: "aAaaaa", status: "Approved", visits: 175 },
+    //   { firstName: "adaSaaa", status: "Cancelled", visits: 165 },
+    //   { firstName: "aasaaa", status: "Cancelled", visits: 157 },
+    //   { firstName: "aweaaaaaewea", status: "Approved", visits: 153 },
+    //   { firstName: "adaAAadsdweaa", status: "Approved", visits: 17585 },
+    //   { firstName: "aAaaaa", status: "Approved", visits: 175 },
+    //   { firstName: "adadsdSaaa", status: "Cancelled", visits: 165 },
+    //   { firstName: "dsdcdaaaaa", status: "Cancelled", visits: 157 },
+    //   { firstName: "aaadvsaa", status: "Submitted", visits: 5153 },
+    //   { firstName: "aaaaswea", status: "Pending", visits: 1555 },
+    //   { firstName: "aaaaauwe", status: "Submitted", visits: 155 }
+    // ];
+    let data = data_;
     this.setState({ data, filteredData: data });
   }
 
